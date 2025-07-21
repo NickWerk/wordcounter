cb4c5205be4ba48bedf8deffaf49883820b1ab9c
ntwrk <nickthequick@gmx.at>
2025-07-21 14:57:50 +0200

    wordcounter.py

--- wordcounter.py
+++ wordcounter.py
@@ -5,13 +5,13 @@ def count_words(pdf_bytes, first_page, last_page):
     reader = fitz.open(stream=pdf_bytes, filetype="pdf")
 
     content = ""
-    for page_number, page in enumerate(reader.pages, start=1):
-        if int(first_page) <= page_number <= int(last_page):
-            content += page.get_text()
+    for page_number in range(reader.page_count):
+    if int(first_page) <= page_number + 1 <= int(last_page):
+        page = reader.load_page(page_number)
+        content += page.get_text()
 
     # Clean up whitespac
     content = " ".join(content.split())
-
     return len(content.split())
 
 st.title("PDF Word Counter")
@@ -20,9 +20,9 @@ st.write("Laden Sie eine PDF-Datei hoch, um die Anzahl der Wörter zu zählen.")
 uploaded_file = st.file_uploader("Wählen Sie eine PDF-Datei aus", type="pdf")
 
 if uploaded_file is not None:
-    pdf_bytes = uploaded_file.read()  # Read the file ONCE
+    pdf_bytes = uploaded_file.read()  # Read the file
     reader = fitz.open(stream=pdf_bytes, filetype="pdf")
-    total_pages = len(reader.pages)
+    total_pages = reader.page_count
     st.write(f"Die PDF-Datei hat insgesamt {total_pages} Seiten.")
 
     # Use a range slider to select the page range
