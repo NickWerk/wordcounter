e54358c6f88a5aecd70dc3bed9412a726de3b6c4
ntwrk <nickthequick@gmx.at>
2025-07-21 14:49:28 +0200

    Update wordcounter.py

--- wordcounter.py
+++ wordcounter.py
@@ -1,23 +1,21 @@
 from sys import argv
-from PyPDF2 import PdfReader
+import fitz
 import streamlit as st
 
 def count_words(upload_pdf, first_page, last_page):
 
-    reader = PdfReader(upload_pdf)
+    reader = fitz.open(upload_pdf)
 
     content = ""
 
     page_number = 1
     for page in reader.pages:
         if page_number >= int(first_page) & page_number <= int(last_page):
-            content += page.extract_text()
+            content += page.get_text()
         page_number += 1
-
     # Remove multiple spaces and newlines
     content = " ".join(content.split())
-
-    return len(content.split(" "))
+    return len(content)
 
 st.title("PDF Word Counter")
 st.write("Laden Sie eine PDF-Datei hoch, um die Anzahl der Wörter zu zählen.")
@@ -25,7 +23,7 @@ st.write("Laden Sie eine PDF-Datei hoch, um die Anzahl der Wörter zu zählen.")
 uploaded_file = st.file_uploader("Wählen Sie eine PDF-Datei aus", type="pdf")
 
 if uploaded_file is not None:
-    reader = PdfReader(uploaded_file)
+    reader = fitz.open(uploaded_file)
     total_pages = len(reader.pages)
     st.write(f"Die PDF-Datei hat insgesamt {total_pages} Seiten.")
 
